c23f29715276d658f2ebc2c1c443d4621e67be48
lelonS <35070147+lelonS@users.noreply.github.com>
2023-10-17 13:46:12 +0200

    Create isStraightFlush tests

---
+++ tests/isStraightFlush.test.js
@@ -0,0 +1,37 @@
+const Hand = require('../Hand');
+const CompareHands = require('../CompareHands');
+const suits = '♥♦♣♠';
+
+test('check that isStraightFlush returns truthy if straight', () => {
+  for (let suit of suits) {
+    let hand = new Hand(suit + '2', suit + '3', suit + '4', suit + '5', suit + '6');
+    expect(CompareHands.isStraightFlush(hand)).toBeTruthy();
+  }
+});
+
+test('check that isStraightFlush returns falsey if not straight', () => {
+  let hand = new Hand('♥9', '♥8', '♥5', '♥7', '♦T');
+  let hand2 = new Hand('♥9', '♥8', '♥5', '♥7', '♥A');
+  expect(CompareHands.isStraightFlush(hand)).toBeFalsy();
+});
+
+test('check that isStraightFlush returns a higher score for a stronger hand (higher straight)', () => {
+  let hand1 = new Hand('♥9', '♥8', '♥5', '♥7', '♥6');
+  let hand2 = new Hand('♣T', '♣9', '♣8', '♣7', '♣6');
+  let hand1Score = CompareHands.isStraightFlush(hand1);
+  let hand2Score = CompareHands.isStraightFlush(hand2);
+  expect(hand2Score).toBeGreaterThan(hand1Score);
+});
+
+test('check that isStraightFlush handles ace as 1 or 14', () => {
+  let hand1 = new Hand('♣A', '♣2', '♣3', '♣4', '♣5');
+  let hand2 = new Hand('♦T', '♦J', '♦Q', '♦K', '♦A');
+  let hand3 = new Hand('♥A', '♥K', '♥Q', '♥2', '♥3');
+  let hand1Score = CompareHands.isStraightFlush(hand1);
+  let hand2Score = CompareHands.isStraightFlush(hand2);
+  let hand3Score = CompareHands.isStraightFlush(hand3);
+  expect(hand1Score).toBeTruthy();
+  expect(hand2Score).toBeTruthy();
+  expect(hand2Score).toBeGreaterThan(hand1Score);
+  expect(hand3Score).toBeFalsy();
+});
