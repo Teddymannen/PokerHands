f03f14ce2874db1a6fdbd91aea378e1ecba5983a
Teddymannen <teddy_kross@hotmail.com>
2023-10-17 14:26:55 +0200

    Update isFourOfAKind.test.js

--- tests/isFourOfAKind.test.js
+++ tests/isFourOfAKind.test.js
@@ -1,10 +1,17 @@
 const Hand = require('../Hand');
 const CompareHands = require('../CompareHands');
 const suits = '♥♦♣♠';
+const ranks = '23456789TJQKA';
+
+function offsetRank(rank) {
+  return rank === 'A' ? '2' : 'A';
+}
 
 test('check that isFourOfAKind returns truthy if four of of a kind', () => {
-  let hand = new Hand('♥2', '♦2', '♣2', '♠2', '♥7');
-  expect(CompareHands.isFourOfAKind(hand)).toBeTruthy();
+  for (let rank of ranks) {
+    let hand = new Hand('♥' + rank, '♦' + rank, '♣' + rank, '♠' + rank, '♥' + offsetRank(rank));
+    expect(CompareHands.isFourOfAKind(hand)).toBeTruthy();
+  }
 });
 
 test('check that isFourOfAKind returns falsey if not four of of a kind', () => {
