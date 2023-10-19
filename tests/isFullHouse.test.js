5bb6b2f0298131b2b7619141ade6fafbd619fc3f
Teddymannen <teddy_kross@hotmail.com>
2023-10-19 10:32:46 +0200

    Add kicker/equal hands tests for isFullHouse

--- tests/isFullHouse.test.js
+++ tests/isFullHouse.test.js
@@ -19,3 +19,20 @@ test('check that isFullHouse returns a higher score for a stronger hand (if two 
   let hand2Score = CompareHands.isFullHouse(hand2);
   expect(hand2Score).toBeGreaterThan(hand1Score);
 });
+
+// Fake tests if only one deck of cards
+test('check that isFullHouse returns a higher score for a stronger hand (if two hands but with higher two of a kind)', () => {
+  let hand1 = new Hand('♥T', '♦T', '♣T', '♠3', '♥3');
+  let hand2 = new Hand('♥T', '♦T', '♣T', '♠7', '♥7');
+  let hand1Score = CompareHands.isFullHouse(hand1);
+  let hand2Score = CompareHands.isFullHouse(hand2);
+  expect(hand2Score).toBeGreaterThan(hand1Score);
+});
+
+test('check that isFullHouse returns a equal score for a equal hand', () => {
+  let hand1 = new Hand('♥T', '♦T', '♣T', '♠3', '♥3');
+  let hand2 = new Hand('♥T', '♦T', '♣T', '♠3', '♥3');
+  let hand1Score = CompareHands.isFullHouse(hand1);
+  let hand2Score = CompareHands.isFullHouse(hand2);
+  expect(hand2Score).toEqual(hand1Score);
+});
